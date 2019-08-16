25d1028218d8c512927a0c53161a07f1d5c00591
Tom Slade <51721274+tomslade1@users.noreply.github.com>
2019-08-16 18:45:29 +0100

    Added conda env file

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from json import dumps
 from flask_jsonpify import jsonify
 
 db_connect = create_engine('sqlite:///chinook.db')
-table_names = db_connect.table_names()
 
-print(table_names)
+app = Flask(__name__)
+api = Api(app)
